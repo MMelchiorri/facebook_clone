88793a5fe84f5e9c9bbdb2fb3d15f6a1504b9273
Marco <mm.melchiorri@gmail.com>
2023-07-10 19:51:08 +0200

    added some style to UserAction

--- src/atoms/UserAction.js
+++ src/atoms/UserAction.js
@@ -1,15 +1,24 @@
 import { createUseStyles } from "react-jss"
 
 const useStyles = createUseStyles({
-    userAction:{
+  userAction: {
+    marginLeft:9,
+    padding:10,
+    backgroundColor:'#F0F2F5',
+    height:20,
+    borderRadius:20,
+    display:"flex",
+    alignItems:'center'
 
-    }
+  }
 })
 
 
-export const UserAction = ({icon}) => {
-    const classes = useStyles()
+export const UserAction = ({ icon }) => {
+  const classes = useStyles()
   return (
-    <div className={classes.userAction}>{icon}</div>
+    <div className={classes.userAction}>
+      {icon}
+    </div>
   )
 }
--- src/organisms/NavBar.js
+++ src/organisms/NavBar.js
@@ -1,30 +1,35 @@
-import { GroupsIcon, FriendsIcon, PlayIcon, HomeIcon,MessengerIcon,ProfileIcon,NotificationIcon,MenuIcon} from "../assets/icons"
+import { GroupsIcon, FriendsIcon, PlayIcon, HomeIcon, MessengerIcon, ProfileIcon, NotificationIcon, MenuIcon } from "../assets/icons"
 import NavAction from "../atoms/NavAction"
 import { UserAction } from "../atoms/UserAction"
 import FacebookLogo from "../atoms/FacebookIcon"
 import { createUseStyles } from 'react-jss'
 
-const NavActions = [ { icon: HomeIcon, route: 'home' },{ icon: FriendsIcon, route: 'friends' }, { icon: PlayIcon, route: 'watch' }]
+const NavActions = [{ icon: HomeIcon, route: 'home' }, { icon: FriendsIcon, route: 'friends' }, { icon: PlayIcon, route: 'watch' }]
 
-export const UserActions = [{ icon: MenuIcon, route: 'menu' },{ icon: MessengerIcon, route: 'messenger' }, { icon: NotificationIcon, route: 'notification' },{icon:ProfileIcon,path:'profile'}]
+export const UserActions = [{ icon: MenuIcon, route: 'menu' }, { icon: MessengerIcon, route: 'messenger' }, { icon: NotificationIcon, route: 'notification' }, { icon: ProfileIcon, path: 'profile' }]
 
 const useStyles = createUseStyles({
-    nav:{
+    nav: {
         padding: 8,
-        maxHeight:40,
-        display:'flex',
-        justifyContent:'flex-start',
-        alignItems:'center',
-        backgroundColor:'#FFFFFF',
+        maxHeight: 40,
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
         boxShadow: '0 1px 2px 0 rgba(0,0,0,.2)'
 
+    },
+
+    userSection:{
+        display:"flex",
+        marginLeft:700,
     }
 })
 
 
 const NavBar = () => {
 
-    const classes=useStyles()
+    const classes = useStyles()
 
     return (
         <div className={classes.nav}>
@@ -32,9 +37,11 @@ const NavBar = () => {
             {NavActions.map((item, index) => {
                 return <NavAction icon={<item.icon />} />
             })}
-            {UserActions.map((item,index)=>{
-                return <UserAction icon={<item.icon />}/>
-            })}
+            <div className={classes.userSection}>
+                {UserActions.map((item, index) => {
+                    return <UserAction icon={<item.icon />} />
+                })}
+            </div>
         </div>
     )
 }
