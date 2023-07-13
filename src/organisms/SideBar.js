b1a02a1757e3d25fabc863114d12050376dc29fc
Gatsufy <mm.melchiorri@gmail.com>
2023-07-13 09:58:25 +0200

    completed sidebar

--- src/organisms/SideBar.js
+++ src/organisms/SideBar.js
@@ -1,24 +1,25 @@
 import { createUseStyles } from "react-jss"
-import { FriendIcon, GroupIcon, MarketPlaceIcon, MemoriesIcon, PlayIcon, ProfileIcon } from "../assets/icons"
+import { BookMark, FriendIcon, GroupIcon, MarketPlaceIcon, MemoriesIcon, PlayIcon, ProfileIcon } from "../assets/icons"
 import SideBarAction from "../atoms/SideBarAction"
 
-const array_sidebar = [{text:'Marco Melchiorri',icon:ProfileIcon},{text:'Amici',icon:FriendIcon},{text:'Ricordi',icon:MemoriesIcon},{text:'Gruppi',icon:GroupIcon},{text:'Watch',icon:PlayIcon},{text:'MarketPlace',icon:MarketPlaceIcon}]
+const array_sidebar = [{ text: 'Marco Melchiorri', icon: ProfileIcon }, { text: 'Amici', icon: FriendIcon }, { text: 'Ricordi', icon: MemoriesIcon }, { text: 'Gruppi', icon: GroupIcon }, { text: 'Watch', icon: PlayIcon }, { text: 'MarketPlace', icon: MarketPlaceIcon },{text:'Elementi Salvati',icon:BookMark}]
 
 const useStyles = createUseStyles({
-    wrapper:{
-        display:'flex',
-        flexDirection:'column',
-        gap:8,
-        padding:16
-    }
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 24,
+    padding: 16,
+    width:300,
+  }
 
 })
 const SideBar = () => {
-    
-    const classes = useStyles()
+
+  const classes = useStyles()
   return (
     <div className={classes.wrapper}>
-      {array_sidebar.map((element,index)=>{
+      {array_sidebar.map((element, index) => {
         return <SideBarAction text={element.text} icon={<element.icon />} />
       })}
     </div>
