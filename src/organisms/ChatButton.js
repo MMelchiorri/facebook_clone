c67f2296c93c13952aa8546c6a2f9150b08c30eb
Gatsufy <mm.melchiorri@gmail.com>
2023-07-24 16:50:43 +0200

    removed some components useless

--- src/organisms/ChatButton.js
+++
@@ -1,7 +0,0 @@
-
-
-export const ChatButton = () => {
-  return (
-    <div>ChatButton</div>
-  )
-}
--- src/organisms/NotificationButton.js
+++
@@ -1,7 +0,0 @@
-
-
-export const NotificationButton = () => {
-  return (
-    <div>NotificationButton</div>
-  )
-}
