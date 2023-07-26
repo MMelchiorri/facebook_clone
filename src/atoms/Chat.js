1c7a1c8de9e97015c0baa7e70a7a7edc8e44e71c
Marco <mm.melchiorri@gmail.com>
2023-07-26 19:07:28 +0200

    fixed code

--- src/atoms/Chat.js
+++ src/atoms/Chat.js
@@ -1,15 +1,7 @@
 import { createUseStyles } from "react-jss"
 
 const useStyles = createUseStyles({
-    chatStyle: {
-        position: 'fixed',
-        backgroundColor: 'white',
-        top:58,
-        right: 10,
-        width: 300,
-        height: 500,
-        borderRadius: 20,
-    },
+
 })
 
 export const Chat = () => {
