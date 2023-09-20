785a3cc28a59a940d996f0ce55bd7152e91f84ab
valeffe1109 <valeriofusco1109v@gmail.com>
2023-09-20 17:54:01 +0200

    fix img profile

--- src/atoms/ProfilePhoto.js
+++ src/atoms/ProfilePhoto.js
@@ -1,16 +1,25 @@
 import { createUseStyles } from "react-jss"
 
 const useStyle = createUseStyles({
-    maxWidth:48,
-    maxHeight:48,
-    borderRadius:'100%'
+    wrapper:{
+      maxWidth:48,
+      maxHeight:48,
+      borderRadius:'100%'
+    },
+    imgProfile:{
+      objectFit:'contain',
+      width:'100%',
+      height:'100%',
+      borderRadius:'100%',
+      cursor:'pointer'
+    }
 })
 
 export const ProfilePhoto = ({image}) => {
     const classes = useStyle()
   return (
     
-    <div className={classes.wrapper}>
+    <div className={classes.wrapper} >
         <img className={classes.imgProfile} src={image} alt="profilePhoto"></img>
     </div>
   )
